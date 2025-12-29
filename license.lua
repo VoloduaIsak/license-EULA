16af1487bdf8ba3302b2b9fd5027741f297e4de9
VoloduaIsak <138663928+VoloduaIsak@users.noreply.github.com>
2025-12-29 21:48:38 +0300

    Add files via upload

--- license.lua
+++ license.lua
@@ -1,16 +1,1 @@
-local current_ip = game.GetIPAddress()
-
-local allowed_ips = {
-    --["46.174.53.227:27015"] = true,
-    ["127.0.0.1:27015"] = true,
-    --["loopback"] = true
-}
-
-if allowed_ips[current_ip] then
-    SERVER_HAS_LICENSE = true 
-    
-    print("[License] Сервер " .. current_ip .. " успешно верифицирован!")
-else
-    SERVER_HAS_LICENSE = false
-    print("[License] Сервер " .. current_ip .. " отсутствует в базе.")
-end
+SERVER_HAS_LICENSE = true 
