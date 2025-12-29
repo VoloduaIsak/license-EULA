c57dfeb253ed38cec1296faf4b0e29c3c5240efb
VoloduaIsak <138663928+VoloduaIsak@users.noreply.github.com>
2025-12-29 21:11:59 +0300

    Add files via upload

--- license.lua
+++ license.lua
@@ -1,18 +1,20 @@
-timer.Simple(1, function()
-    game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
-end)
-timer.Simple(2, function()
-    game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
-end)
-timer.Simple(3, function()
-    game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
-end)
-timer.Simple(4, function()
-    game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
-end)
-timer.Simple(5, function()
-    game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
-end)
-timer.Simple(10, function()
-    game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
-end)
+local allowed_ips = {
+    ["123.123.123.123:27015"] = true
+    --["192.168.1.1:27015"] = true,
+    --["0.0.0.0:0"] = true
+}
+
+local current_ip = game.GetIPAddress()
+
+if not allowed_ips[current_ip] then
+    print("--------------------------------------------------")
+    print("ОШИБКА: Лицензия для IP " .. current_ip .. " не найдена!")
+    print("Свяжитесь с владельцем: VoloduaIsak")
+    print("--------------------------------------------------")
+
+    timer.Simple(5, function()
+        RunConsoleCommand("changelevel", "rp_rps_map")
+    end)
+else
+    print("[License] Лицензия подтверждена для сервера: " .. current_ip)
+end
